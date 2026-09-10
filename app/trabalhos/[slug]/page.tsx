c4119398bd7d79697f69c30ab35e2b18013292f3
Wanderson Gandra <gandratecnologia@outlook.com.br>
2026-09-10 00:45:23 -0400

    a11y: expose main content target on project pages

--- app/trabalhos/[slug]/page.tsx
+++ app/trabalhos/[slug]/page.tsx
@@ -33,7 +33,7 @@ export default async function ProjectPage({ params }: { params: Promise<{ slug: 
   return (
     <>
       <Header />
-      <main>
+      <main id="main-content">
         <ProjectView project={project} next={next} />
       </main>
       <Footer />
